9554181eccabd16657f196f3c45e28fbd7ece5d0
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-24 20:31:56 +0800

    refactor: migrate to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,16 @@
+import {defineConfig} from 'tsdown';
+
+export default defineConfig({
+  entry: 'src/main.ts',
+  target: 'es2022',
+  clean: true,
+  dts: true,
+  outputOptions: {
+    minify: {
+      mangle: true,
+      compress: false,
+      removeWhitespace: false,
+      deadCodeElimination: true // TODO remove me
+    }
+  }
+});
