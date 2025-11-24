c8cfe06def94247f7d8c83c20fe5a5f867992b1d
James Garbutt <43081j@users.noreply.github.com>
2025-11-24 09:12:47 +0000

    chore: add licenses to distributed code temporarily

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,25 @@
 import {defineConfig} from 'tsdown';
+import licenses from 'rollup-plugin-license';
 
 export default defineConfig({
   entry: 'src/main.ts',
   target: 'es2022',
   clean: true,
   dts: true,
+  plugins: [
+    licenses({
+      thirdParty: {
+        output: {
+          file: 'dist/LICENSES.txt',
+          template(dependencies) {
+            return dependencies.map((dependency) =>
+              `${dependency.name}:${dependency.version} -- ${dependency.licenseText}`
+            ).join('\n');
+          }
+        }
+      }
+    }) as never
+  ],
   outputOptions: {
     minify: {
       mangle: true,
