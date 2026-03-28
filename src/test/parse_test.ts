c7a7f89426b80a3e315418441932899e6fd5ae7f
Hyper-Z11 <firzeroun11@gmail.com>
2026-03-28 14:42:48 +0700

    test: add Windows tests

--- src/test/parse_test.ts
+++ src/test/parse_test.ts
@@ -14,7 +14,25 @@ describe('parse', () => {
   });
 
   if (isWindows) {
-    // Add Windows tests later
+    // TODO: add more tests
+    const baseOptions = {
+      env: process.env
+    };
+
+    test('just return the same input if resolved', () => {
+      const parsed = parse('node', ['-v'], baseOptions);
+
+      expect(parsed.command).toBe('node');
+      expect(parsed.args).toEqual(['-v']);
+    });
+
+    test('use shell if command are not resolved/available', () => {
+      const parsed = parse('notexist', ['hi'], baseOptions);
+
+      expect(parsed.command.endsWith('cmd.exe')).ok;
+      expect(parsed.args).toEqual(['/d', '/s', '/c', '"notexist ^"hi^""']);
+      expect(parsed.options.windowsVerbatimArguments).toBe(true);
+    });
   } else {
     test('return from arguments', () => {
       expect(parse('node', ['-v'])).toEqual({
