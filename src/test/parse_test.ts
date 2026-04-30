784b9d04692e85c9fdf0b2be7f87b4ae14b81f00
James Garbutt <43081j@users.noreply.github.com>
2026-04-30 15:37:06 +0100

    test: use describe block per os

--- src/test/parse_test.ts
+++ src/test/parse_test.ts
@@ -16,29 +16,30 @@ describe('parse', () => {
     });
   });
 
-  test.runIf(isWindows)('just return the same input if resolved', () => {
-    const parsed = parse('node', ['-v'], baseWindowsOptions);
+  describe.runIf(isWindows)('windows only', () => {
+    test('just return the same input if resolved', () => {
+      const parsed = parse('node', ['-v'], baseWindowsOptions);
 
-    expect(parsed.command).toBe('node');
-    expect(parsed.args).toEqual(['-v']);
-  });
+      expect(parsed.command).toBe('node');
+      expect(parsed.args).toEqual(['-v']);
+    });
 
-  test.runIf(isWindows)(
-    'use shell if command are not resolved/available',
-    () => {
+    test('use shell if command are not resolved/available', () => {
       const parsed = parse('notexist', ['hi'], baseWindowsOptions);
 
       expect(parsed.command.endsWith('cmd.exe')).ok;
       expect(parsed.args).toEqual(['/d', '/s', '/c', '"notexist ^"hi^""']);
       expect(parsed.options.windowsVerbatimArguments).toBe(true);
-    }
-  );
+    });
+  });
 
-  test.runIf(!isWindows)('return from arguments', () => {
-    expect(parse('node', ['-v'])).toEqual({
-      command: 'node',
-      args: ['-v'],
-      options: {}
+  describe.runIf(!isWindows)('unix only', () => {
+    test('return from arguments', () => {
+      expect(parse('node', ['-v'])).toEqual({
+        command: 'node',
+        args: ['-v'],
+        options: {}
+      });
     });
   });
 });
