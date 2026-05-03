fd8ad6f6b86ff344cc37636ac2a19a5dcdc02d1c
hyperz111 <firzeroun11@gmail.com>
2026-05-03 15:11:28 +0700

    deps: remove cross-spawn and its type definition

--- src/types/cross-spawn.d.ts
+++
@@ -1,9 +0,0 @@
-import type {SpawnOptions} from 'child_process';
-
-declare module 'cross-spawn' {
-  export function _parse(
-    file: string,
-    args: readonly string[],
-    options?: SpawnOptions
-  ): {command: string; args: string[]; options: SpawnOptions};
-}
